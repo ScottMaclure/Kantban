6696bf56c90cf771ac6b2fd805886e7a68bb8084
Martien Verbruggen <martien.verbruggen@gmail.com>
2011-06-06 09:10:54 +1000

    refactor names of controllers. Factor Ajax back in with normal controller methods.

---
+++ app/controllers/Project.java
@@ -0,0 +1,91 @@
+package controllers;
+
+import javax.annotation.Nonnull;
+
+import models.ResponseMessage;
+import models.State;
+import models.Story;
+
+import org.apache.log4j.Logger;
+
+import play.mvc.Controller;
+import play.mvc.With;
+
+@With(Secure.class)
+public class Project extends Controller {
+	
+	private final static Logger log = Logger.getLogger(Project.class);
+	
+	/*
+	 * This can be used by any controller that only needs the project 
+	 * set in the model. 
+	 * @param id the project id
+	 */
+	private static void genericProject(@Nonnull Long id) {
+    	models.Project project = models.Project.findById(id);
+    	render(project);
+	}
+
+    public static void index(@Nonnull Long id) {
+    	genericProject(id);
+    }
+
+    public static void board(@Nonnull Long id) {
+    	genericProject(id);
+    }
+    
+    public static void process(@Nonnull Long id) {
+    	genericProject(id);
+    }
+
+    public static void story(@Nonnull Long id) {
+    	genericProject(id);
+    }
+
+	public static void moveState(Long stateId, Integer index) {
+		log.debug("moveState(" + stateId + ", " + index + ")");
+		ResponseMessage r = new ResponseMessage(false);
+		State state = State.findById(stateId);
+		if (state != null) {
+			models.Project project = state.project;
+			r.setSuccess(project.moveState(state, index));
+			project.save();
+		}
+		else {
+			r.addMessage("Could not find state " + stateId);
+		}
+		renderJSON(r);
+	}
+	
+	public static void newState(Long projectId, String name, String description, Integer limit) {
+		log.debug("newState(" + projectId + ", " + name + ", " + description + ", " + limit + ")");
+		ResponseMessage r = new ResponseMessage(false);
+		models.Project project = models.Project.findById(projectId);
+		if (project != null) {
+			project.newState(name, description, limit);
+			project.save();
+			r.setSuccess(true);
+		}
+		else {
+			r.addMessage("Couldn't find project with id " + projectId);
+		}
+		renderJSON(r);
+	}
+	
+	public static void deleteState(Long stateId) {
+		log.debug("deleteState(" + stateId + ")");
+		ResponseMessage r = new ResponseMessage(false);
+		State state = State.findById(stateId);
+		if (state != null) {
+			models.Project project = state.project;
+			r.setSuccess(project.removeState(state));
+			project.save();
+			state.delete();
+		}
+		else {
+			r.addMessage("Could not find state " + stateId);
+		}
+		renderJSON(r);
+	}
+
+}
---
+++ app/controllers/State.java
@@ -0,0 +1,59 @@
+package controllers;
+
+import models.ResponseMessage;
+import models.Story;
+
+import org.apache.log4j.Logger;
+
+import play.mvc.Controller;
+
+public class State extends Controller {
+	
+	private final static Logger log = Logger.getLogger(State.class);
+
+	/**
+	 * Move a story to a target state, with position index
+	 * 
+	 * @param storyId
+	 * @param StateId
+	 * @param index
+	 */
+	public static void moveStoryToState(Long storyId, Long stateId, Integer index) {
+		log.debug("moveStoryToState(" + storyId + ", " + stateId + ", " + index + ")");
+		ResponseMessage r = new ResponseMessage(false);
+		Story story = Story.findById(storyId);
+		models.State state = models.State.findById(stateId);
+		if (story != null && state != null) {
+			state.stories.add(story);
+			r.setSuccess(state.moveStory(story, index));
+			state.save();
+		}
+		else {
+			r.addMessage("Could not find story " + storyId + " or state " + stateId);
+		}
+		renderJSON(r);
+	}
+
+	/**
+	 * Move a story in the state order to position index
+	 * 
+	 * @param storyId
+	 * @param StateId
+	 * @param index
+	 */
+	public static void moveStory(Long storyId, Integer index) {
+		log.debug("moveStory(" + storyId + ", " + index + ")");
+		ResponseMessage r = new ResponseMessage(false);
+		Story story = Story.findById(storyId);
+		if (story != null) {
+			models.State state = story.state;
+			r.setSuccess(state.moveStory(story, index));
+			state.save();
+		}
+		else {
+			r.addMessage("Could not find story " + storyId);
+		}
+		renderJSON(r);
+	}
+	
+}
